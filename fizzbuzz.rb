3b6512562241709de97da24289e07c6208a6e0a5
Josh Mills <josh@trueheart78.com>
2014-12-27 15:54:42 -0500

    Passing in 3 now returns 'Fizz'

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,7 +3,11 @@ class FizzBuzz
 		@value = value
 	end
   def fizz
-  	@value
+  	if @value == 3
+  		"Fizz"
+  	else
+  		@value
+  	end
   end
 end
 
@@ -14,6 +18,9 @@ class FizzBuzzTest < Test::Unit::TestCase
 
 	def test_should_return_number
 		assert_equal(1, FizzBuzz.new(1).fizz)
+	end
+	def test_three_should_get_fizz
+		assert_equal("Fizz", FizzBuzz.new(3).fizz)
 	end
 
 end
